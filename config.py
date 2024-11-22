77a5e404e51f67adbbf0f477be6367e97d2a2798
NYBotx <167092531+NYBotx@users.noreply.github.com>
2024-11-22 09:30:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import re
 id_pattern = re.compile(r'^.\d+$')
 
 # bot
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '13963336'))
+API_HASH = environ.get('API_HASH', 'a144d1e22ef0b29738e8c00713d02678')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7792954183:AAFn0Z33a_FobtcxBRrTpfibd6Ax6Ok11ck')
 ADMINS = [int(admins) if id_pattern.search(admins) else admins for admins in environ.get('ADMINS', '1867106198').split()]
 
 # bs
@@ -16,4 +16,4 @@ FSUB_ID = int(fsub_eid) if fsub_eid and id_pattern.search(fsub_eid) else None
 
 # database
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
